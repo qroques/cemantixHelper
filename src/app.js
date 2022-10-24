ccf843fc945c4659e1e18eea19dd8e9094482ce5
Quentin Roques <quentin.roques@knplabs.com>
2022-10-24 16:39:07 +0200

    feat: update to use real script (#1)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,78 @@
-alert("It works!");
+const DICTIONNARY = [
+	'concurrence',
+	'marché',
+	'monopole',
+	'consommateur',
+	'client',
+	'produit',
+	'clientèle',
+	'entreprise',
+	'acheteur',
+	'cible',
+	'publicité',
+	'offre',
+	'négocier',
+	'part',
+	'marketing',
+	'transaction',
+	'producteur',
+	'vente',
+	'vendre',
+	'vendeur',
+	'négociation',
+	'spéculation',
+	'capitalisme',
+	'consommation',
+	'marchand',
+	'magasin',
+	'appareil',
+	'test',
+	'acheter',
+	'outil',
+	'marchandage',
+	'export',
+	'avion',
+	'négociant',
+	'trader',
+	'clé',
+	'consommer',
+	'voiture',
+	'verrou',
+	'patient',
+	'contrat',
+	'pack',
+	'annonce',
+	'consumérisme',
+	'promouvoir',
+	'objet',
+	'occasion',
+	'promotion',
+	'import',
+	'roue',
+	'action',
+	'constitution',
+	'carte',
+	'bourse'
+];
+
+
+const trigger = document.getElementById('puzzle-num');
+const summary = document.getElementById('cemantix-summary');
+const helpMeButton = document.createElement('button');
+
+helpMeButton.id = 'help-me-button';
+helpMeButton.innerHTML = 'Help Me Obi Wan Kenobi!'
+summary.appendChild(helpMeButton);
+helpMeButton.addEventListener('click', helpMe); 
+const form = document.getElementById('cemantix-form');
+const guessInput = document.getElementById('cemantix-guess');
+const guessButton = document.getElementById('cemantix-guess-btn');
+function helpMe() {
+	DICTIONNARY.forEach(element => {
+		guessInput.value = element;
+		guessButton.click();
+		setTimeout(function(){
+			console.log("sending " + element);
+		}, 10000);
+	});
+}
